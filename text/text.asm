801e3ea8347bbd72aed2e410077c7e44a4ff6aca
amemorystore <76083273+amemorystore@users.noreply.github.com>
2023-02-14 17:11:28 -0500

    Initial commit

---
+++ text/text.asm
@@ -0,0 +1,13 @@
+; this is a simple text:
+;.
+_Text::
+	text " this is some"
+	line " text something"
+	cont "  might read."
+
+	para " this is some"
+	line " other text"
+	cont "  something"
+	cont "might read."
+	done
+;. end
